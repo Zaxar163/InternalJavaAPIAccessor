e7145866b66ee0a007c267c498b4e266ac68fb49
zaxar163 <zahar.vcherachny@yandex.ru>
2019-05-14 19:31:34 +0300

    Works all time.

--- src/main/java/ru/zaxar163/core/JVMPlayGround.java
+++ src/main/java/ru/zaxar163/core/JVMPlayGround.java
@@ -8,25 +8,22 @@ import java.util.function.Supplier;
 import ru.zaxar163.unsafe.fast.FastUtil;
 import ru.zaxar163.unsafe.fast.InvokerMethod;
 import ru.zaxar163.unsafe.fast.ReflectionUtil;
-import ru.zaxar163.unsafe.fast.proxies.ProxyList;
 import ru.zaxar163.unsafe.xlevel.ThreadList;
 
 public final class JVMPlayGround {
+	private static InvokerMethod classConstructor = null;
+	private static final Supplier<Object> clazzSameSize = ReflectionUtil
+			.sameSizeObject(DelegateClassLoader.INSTANCE, Class.class, Collections.emptyList());
 	public static void main(final String... args) throws Throwable {
-		Class.forName(ProxyList.class.getName());
-		Class.forName(ReflectionUtil.class.getName());
 		ThreadList.getThreads().forEach((n, t) -> {
 			System.out.println("Thread # " + n + " Data: " + t + " Classloader: " + t.getContextClassLoader());
 		});
+		init();
 		try {
-			final Supplier<Object> classSameInstancer = ReflectionUtil
-					.sameSizeObject(ClassLoader.getSystemClassLoader(), Class.class, Collections.emptyList());
-			final InvokerMethod clI = ReflectionUtil
-					.wrapMethod(ReflectionUtil.methodify(Class.class.getDeclaredConstructors()[0]));
-			final Class<?> a = ReflectionUtil.changeObjUnsafe(Class.class, classSameInstancer.get());
-			clI.invoke(a, ClassLoader.getSystemClassLoader());
+			final Class<?> a = newClazz();
+			classConstructor.invoke(a, ClassLoader.getSystemClassLoader());
 			System.out.println(a.getClassLoader());
-			clI.invoke(a, new URLClassLoader(new URL[0]));
+			classConstructor.invoke(a, new URLClassLoader(new URL[0]));
 			System.out.println(a.getClassLoader());
 		} catch (final Throwable e) {
 			e.printStackTrace();
@@ -34,6 +31,45 @@ public final class JVMPlayGround {
 		System.out.println(FastUtil.fastEquals(new byte[] { 2, 5, 5, 6, 7 }, new byte[] { 2, 5, 5, 7, 7 }));
 		System.out.println(FastUtil.fastEquals(new byte[] { 2, 5, 5, 6, 7 }, new byte[] { 2, 5, 5, 6, 7 }));
 		// Crasher.crashZip();
+	}
+	
+	public static Class<?> newClazz() {
+		return ReflectionUtil.changeObjUnsafe(Class.class, clazzSameSize.get());
+	}
+	
+	public static void init() {
+		try {
+			final InvokerMethod clI = ReflectionUtil
+					.wrapMethod(ReflectionUtil.methodify(Class.class.getDeclaredConstructors()[0]));
+			final Class<?> a = newClazz();
+			if (ClassUtil.JAVA9)
+				clI.invoke(a, ClassLoader.getSystemClassLoader(), null);
+			else
+				clI.invoke(a, ClassLoader.getSystemClassLoader());
+			classConstructor = clI;
+		} catch (final Throwable e) {
+			//e.printStackTrace();
+			init();
+		}
+	}
+	
+	public static void constructClazz(Class<?> clazz, Object... args) {
+		try {
+			classConstructor.invoke(clazz, args);
+		} catch (Throwable e) {
+			throw new Error(e);
+		}
+	}
+	
+	public static void constructClazzU(Class<?> clazz, ClassLoader loader) {
+		try {
+			if (ClassUtil.JAVA9)
+				classConstructor.invoke(clazz, loader, null);
+			else
+				classConstructor.invoke(clazz, loader);
+		} catch (Throwable e) {
+			throw new Error(e);
+		}
 	}
 
 	private JVMPlayGround() {
