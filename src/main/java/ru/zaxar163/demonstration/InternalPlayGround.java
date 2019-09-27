521477446cc004b465e4061a3db3a885a2cac720
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-09-27 19:15:35 +0200

    Small fix.

--- src/main/java/ru/zaxar163/demonstration/InternalPlayGround.java
+++ src/main/java/ru/zaxar163/demonstration/InternalPlayGround.java
@@ -1,13 +1,17 @@
 package ru.zaxar163.demonstration;
 
+import java.util.Arrays;
+
 import ru.zaxar163.unsafe.xlevel.ThreadList;
+import ru.zaxar163.util.proxies.ProxyList;
 
 public final class InternalPlayGround {
 	public static void main(final String... args) throws Throwable {
 		System.out.println("Get list of all threads in JVM excluding JVM management thread:");
 		ThreadList.getThreads().forEach((n, t) -> {
-			System.out.println("Thread # " + n + " Data2: " + t + " Classloader: " + t.getContextClassLoader());
+			System.out.println("Thread # " + n + " Data: " + t + " Classloader: " + t.getContextClassLoader());
 		});
+		System.out.println(Arrays.toString(ProxyList.THREAD.getThreads()));
 		JVMPlayGround.main(args);
 	}
 
